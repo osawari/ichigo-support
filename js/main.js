795edf938fe54fa8d1b50051cccd6250fa99f04e
osawari <103305496+osawari@users.noreply.github.com>
2026-02-24 00:53:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 'use strict';
 
 {
+  const toast = document.querySelector("#toast");
 
-  
+  function showToast(message) {
+    toast.textContent = message;
+    toast.classList.add("show");
 
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 2000);
+  }
+
+  document.querySelectorAll(".copyable").forEach(item => {
+    item.addEventListener("click", () => {
+      navigator.clipboard.writeText(item.textContent);
+      showToast("コピーしました ✅");
+    });
+  });
 }
